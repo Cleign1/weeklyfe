ffdeeb7b8fbd1caa62cb9b538c42e4342c61f75c
Cleign <ibnukhaidar@gmail.com>
2023-05-16 01:22:49 +0700

    on sync with the instructor

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NotFound from "./pages/NotFound";
-
+import NotFound from "./Pages/NotFound";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<>Home Page</>}/>
-        <Route path="/login" element={<>login</>}/>
-        <Route path="/register" element={<>Register</>}/>
+        <Route path="/" element={<>Home Page</>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
-        <Route path="*" element={<NotFound/>}/>
-
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
